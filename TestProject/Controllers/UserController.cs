b3a234dca817e8e51948c8a055d2435f69fbc023
Valeriia <ya.lera.amazon@gmail.com>
2023-04-23 13:27:50 +0200

    Added UserRepository to controller

--- TestProject/Controllers/UserController.cs
+++ TestProject/Controllers/UserController.cs
@@ -1,31 +1,71 @@
 using Microsoft.AspNetCore.Mvc;
 using Work.ApiModels;
+using Work.Database;
+using Work.Interfaces;
 
 namespace Work.Controllers
 {
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly IRepository<User, Guid> _userRepository;
+        private const string UserNotExistMessage = "User does not exist";
+
+        public UserController(IRepository<User, Guid> userRepository)
+        {
+            _userRepository = userRepository;
+        }
 
         public IActionResult Get(Guid id)
         {
-            throw new NotImplementedException("TODO");
+            try
+            {
+                var user = _userRepository.Read(id);
+                return Ok(user);
+            }
+            catch
+            {
+                return NotFound();
+            }
         }
 
         public IActionResult Post(UserModelDto user)
         {
-            throw new NotImplementedException("TODO");
+            try
+            {
+                _userRepository.Update(new User { UserId = user.Id, UserName = user.Name, Birthday = user.Birthdate });
+                return Accepted();
+            }
+            catch
+            {
+                return BadRequest(UserNotExistMessage);
+            }
         }
-        
+
         public IActionResult Put(UserModelDto user)
         {
-            throw new NotImplementedException("TODO");
+            try
+            {
+                _userRepository.Create(new User { UserId = user.Id, UserName = user.Name, Birthday = user.Birthdate });
+                return Accepted();
+            }
+            catch
+            {
+                return BadRequest(UserNotExistMessage);
+            }
         }
 
         public IActionResult Delete(Guid id)
         {
-            throw new NotImplementedException("TODO");
+            try
+            {
+                _userRepository.Remove(new User { UserId = id });
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest(UserNotExistMessage);
+            }
         }
-
     }
 }
